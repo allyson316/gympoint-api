176577269adc3a24a6cd401f277850ae0fd20911
Allyson Andrade <allyson316@gmail.com>
2019-12-02 22:33:12 -0300

    ajuste no nome da aplicação

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 import app from './app';
 
 app.listen(process.env.NODE_PORT, () =>
-  console.log(`\n API: "Meetapp - API"
+  console.log(`\n API: "Gympoint - API"
  Running on port: ${process.env.NODE_PORT}
  Environment: ${process.env.NODE_ENV}`)
 );
